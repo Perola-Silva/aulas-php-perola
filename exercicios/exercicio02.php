c2be7c62c2c526a58d7b4c7e861a477719a4eeec
Perola-Silva <perolladasillva@gmail.com>
2026-08-10 09:17:16 -0300

    adiconado bootstrap nos exercicios

--- exercicios/exercicio02.php
+++ exercicios/exercicio02.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 02 (corrigido)</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
+
     <style>
         article {
             padding: 2px;
@@ -62,6 +66,9 @@ Veja o uso da setinha e das propriedades -->
         <p>Idade: <?= $usuario2->idade ?> anos</p>
     </article>
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
