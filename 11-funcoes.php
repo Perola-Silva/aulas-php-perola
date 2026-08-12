12e8431191b3d97a0b691783cac12f9a110497d7
Perola-Silva <perolladasillva@gmail.com>
2026-08-12 11:37:49 -0300

    .

--- 11-funcoes.php
+++ 11-funcoes.php
@@ -6,15 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funções</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/CSS/bootstrap.min (1).css">
 
-    <!-- <style>
+    <style>
         body {
             font-family: cursive;
-            background-color: #0c0c0c;
-            color: #ebdddd;
         }
-    </style> -->
+    </style>
 </head>
 
 <body>
