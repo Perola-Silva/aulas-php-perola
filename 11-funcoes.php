6acef3a78a9f9611fb07edfd3ddb6348dd3ea876
Perola-Silva <perolladasillva@gmail.com>
2026-08-12 11:29:22 -0300

    adicionado exemplo de função anônima (ou lambda)

--- 11-funcoes.php
+++ 11-funcoes.php
@@ -99,6 +99,20 @@ function exibirDadosDoAutor(){
         <p>Número 10: <?= verificarNegativo(10) ?></p>
         <p>Número -10: <?= verificarNegativo(-10) ?></p>
 
+        <hr>
+
+        <h2>Função anônima (ou lambda)</h2>
+
+        <?php 
+        // SEMPRE DECLARE primeiro a função antes de chamar
+        $multiplicar = function(float $valor1, float $valor2){
+            return $valor1 * $valor2;
+        };
+        ?>
+
+        <!-- Repare o uso do $ junto com o nome da função: -->
+        <p>Exemplo: <?= $multiplicar(10, 2) ?></p>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
