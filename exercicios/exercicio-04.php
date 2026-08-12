6cfe83e09bd9dcbf9c8b4a921b35a06379f07467
Perola-Silva <perolladasillva@gmail.com>
2026-08-12 09:03:38 -0300

    fazido

--- exercicios/exercicio-04.php
+++ exercicios/exercicio-04.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,50 +9,84 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 
     <style>
-        body{
+        body {
             font-family: cursive;
-            background-color: #080000;
+            background-color: #0c0c0c;
             color: aliceblue;
+        }
+
+        .table-custom-striped tbody tr:nth-child(even) td {
+            background-color: #fcdada; 
+            color: #000000;             
         }
     </style>
 </head>
+
 <body>
+    <div class="container">
     <h1>Exercício 04: estruturas de repetição (loops) e estruturas de dados</h1>
+    <hr>
 
-<?php 
-$linguagens = [
-    ["id" => 1,
-    "linguagem" => "HTML",
-    "funcao" => "Estruturação"],
+    <?php
+    $linguagens = [
+        [
+            "id" => 1,
+            "linguagem" => "HTML",
+            "funcao" => "Estruturação"
+        ],
 
-    ["id" => 2,
-    "linguagem" => "CSS",
-    "funcao" => "Estilos"],
+        [
+            "id" => 2,
+            "linguagem" => "CSS",
+            "funcao" => "Estilos"
+        ],
 
-    ["id" => 3,
-    "linguagem" => "JS",
-    "funcao" => "Comportamentos"],
+        [
+            "id" => 3,
+            "linguagem" => "JS",
+            "funcao" => "Comportamentos"
+        ],
 
-    ["id" => 4,
-    "linguagem" => "PHP",
-    "funcao" => "Back-End"],
+        [
+            "id" => 4,
+            "linguagem" => "PHP",
+            "funcao" => "Back-End"
+        ],
 
-    ["id"=> 5,
-    "linguagem" => "SQL",
-    "funcao" => "Manipulação de dados"]
-];
+        [
+            "id" => 5,
+            "linguagem" => "SQL",
+            "funcao" => "Manipulação de dados"
+        ]
+    ];
+    ?>
 
-foreach($linguagens as $linguagem):
-?>
+    <table class="table table-bordered border-light table-custom-striped">
 
-<p><?= $linguagem["id"] ?></p>
-<p><?= $linguagem["linguagem"] ?></p>
-<p><?= $linguagem["funcao"] ?></p>
+        <tr class="table-dark">
+            <th>ID</th>
+            <th>Linguagem</th>
+            <th>Função</th>
+        </tr>
 
+        <?php
+        foreach ($linguagens as $linguagem):
+        ?>
 
-<?php 
-endforeach;
-?>
+            <tr>
+                <td><?= $linguagem["id"] ?></td>
+                <td><?= $linguagem["linguagem"] ?></td>
+                <td><?= $linguagem["funcao"] ?></td>
+            </tr>
+
+        <?php
+        endforeach;
+        ?>
+
+    </table>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+
 </html>
