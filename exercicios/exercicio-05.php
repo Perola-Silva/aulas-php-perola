51679217c4bb74f4ff09073798ca30b1f61bba54
Perola-Silva <perolladasillva@gmail.com>
2026-08-14 09:56:12 -0300

    .

--- exercicios/exercicio-05.php
+++ exercicios/exercicio-05.php
@@ -7,38 +7,33 @@
 
     <link rel="stylesheet" href="/CSS/bootstrap.min (1).css">
 
-    <style>
-        body {
-            font-family: cursive;
-        }
-    </style>
 </head>
 <body>
     <div class="container">
     <h1>Exercício 05: funções</h1>
 
-    <hr>
-
     <?php 
-    function calculoMedia(float $nota1, float $nota2, float $nota3){
-        return $media = ($nota1 + $nota2 + $nota3) / 3;
+    $nota1 = 7;
+    $nota2 = 3;
+    $nota3 = 6;
+
+    function calculoMedia(float $nota1, float $nota2, float $nota3):float{
+        return ($nota1 + $nota2 + $nota3) / 3;
     };
+
+    $media = calculoMedia($nota1, $nota2, $nota3);
+
+    function situacao(float $media):string{
+        if($media >= 7 ) return  "Aprovado";
+        return  "Reprovado";
+    };
+
+    $situacao = situacao($media);
+    $cor = ($situacao == "Aprovado") ? "text-info" : "text-danger";
     ?>
 
-    <p><?= calculoMedia(10, 7, 5) ?></p>
-    
-
-    <?php 
-    function situacaoAluno($media){
-        if($media >= 7 ){
-            return $situacao = "Aprovado";
-        } else {
-            return $situacao = "Reprovado";
-        }
-    }
-    ?>
-
-    <p><?= situacaoAluno() ?></p>
+    <p><?= $media ?></p>
+    <p class="<?= $cor ?>"><?= $situacao ?></p>
 
     </div>
 </body>
