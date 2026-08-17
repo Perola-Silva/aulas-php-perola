ffd2629b641daaab1a9d423c21bbaa4533bef20c
Perola-Silva <perolladasillva@gmail.com>
2026-08-17 11:23:54 -0300

    exemplo array_sum()

--- 14-funcoes-nativas-para-arrays.php
+++ 14-funcoes-nativas-para-arrays.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,6 +44,21 @@
         </ul>
 
         <p><?= $nome ?></p>
+
+        <hr>
+
+        <h2><code>array_sum()</code></h2>
+        <p>Somando os valores de um array</p>
+
+        <?php 
+        $carrinhoDeCompras = [
+            "TV_led" => 1200,
+            "Ultrabook" => 2500,
+            "Geladeira" => 3000
+        ];
+
+        $total = array_sum($carrinhoDeCompras); 
+        ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
