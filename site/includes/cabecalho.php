ec41240c15d22e55e9a31ff7ccee31b8481695b5
Perola-Silva <perolladasillva@gmail.com>
2026-08-19 11:38:27 -0300

    fazido coisas

---
+++ site/includes/cabecalho.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site usando PHP</title>
+    <link rel="stylesheet" href="/CSS/bootstrap.min (1).css">
+
+</head>
+<body>
+    <header>
+        <h1>Site com PHP</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="cursos.php">Cursos</a>
+            <a href="duvidas.php">Dúvidas</a>
+        </nav>
+    </header>
+
+    <main>
