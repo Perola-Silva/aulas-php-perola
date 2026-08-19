e43ef2080911cc31df3bf7bd643df63aca3a0dc9
Perola-Silva <perolladasillva@gmail.com>
2026-08-19 08:25:26 -0300

    exemplo de array_column()

--- 14-funcoes-nativas-para-arrays.php
+++ 14-funcoes-nativas-para-arrays.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funções para Arrays</title>
     <link rel="stylesheet" href="/CSS/bootstrap.min (1).css">
 </head>
+
 <body>
     <div class="container">
         <h1>Funções nativas para Arrays</h1>
@@ -15,7 +17,7 @@
         <h2><code>implode()</code></h2>
         <p>Transforma array em uma string.</p>
 
-        <?php 
+        <?php
         $arrayBandas = ["Red Hot Chilli Pepers", "Green Day", "Metalica"];
         $textoBandas = implode("-", $arrayBandas);
         ?>
@@ -28,7 +30,7 @@
         <h2><code>extract()</code></h2>
         <p>Extrai chaves associativas para variáveis.</p>
 
-        <?php 
+        <?php
         $nome = "Beltrano";
 
         $aluno = ["id" => 1, "nome" => "Fulano", "idade" => 25];
@@ -50,14 +52,14 @@
         <h2><code>array_sum()</code></h2>
         <p>Somando os valores de um array</p>
 
-        <?php 
+        <?php
         $carrinhoDeCompras = [
             "TV_led" => 1200,
             "Ultrabook" => 2500,
             "Geladeira" => 3000
         ];
 
-        $total = array_sum($carrinhoDeCompras); 
+        $total = array_sum($carrinhoDeCompras);
         ?>
 
         <p>Total: <?= $total ?></p>
@@ -65,10 +67,10 @@
         <h2><code>array_unique()</code></h2>
         <p>Gera umm novo array removendo elementos duplicados/repetidos um um array.</p>
 
-        <?php 
+        <?php
         $categorias = ["eletronicos", "livros", "roupas", "games", "eletronicos", "livros"];
 
-        $categoriasUnicas = array_unique($categorias); 
+        $categoriasUnicas = array_unique($categorias);
         ?>
 
         <pre><?php var_dump($categorias) ?></pre>
@@ -79,7 +81,7 @@
         <h2><code>array_merge()</code></h2>
         <p>Junta dados de arrays diferentes.</p>
 
-        <?php 
+        <?php
         $produtosFilialNorte = ["Mouse", "Teclado"];
         $produtosFilialSul = ["Monitor", "Webcam", "Pendrive", "Teclado"];
 
@@ -97,7 +99,7 @@
 
         <p>Cria um novo array a partir uma lista de valores e uma lista de chaves.</p>
 
-        <?php 
+        <?php
         // Lista de chaves
         $games = ["Super_Mario", "Sonic", "Final_Fantasy"];
 
@@ -115,15 +117,36 @@
         <h2><code>array_map()</code></h2>
         <p>Percorre cada elemnto de um array, executa uma função (chamada de callback) e gera um novo array com os resultados.</p>
 
-        <?php 
-        $catatlogoComDesconto = array_map(function($preco){
+        <?php
+        $catatlogoComDesconto = array_map(function ($preco) {
             return $preco - $preco * 0.1;
         }, $catalogo);
         ?>
 
         <pre><?php var_dump($catatlogoComDesconto) ?></pre>
 
+        <hr>
+
+        <h2><code>array_column()</code></h2>
+
+        <p>Retorna um novo array com valores de uma determinada chave associativa.</p>
+
+        <?php
+        $servicos = [
+            ["codigo" => 3, "tipo" => "Limpeza", "status" => "Concluído"],
+            ["codigo" => 12, "tipo" => "Manutenção", "status" => "Concluído"],
+            ["codigo" => 1, "tipo" => "Reparo", "status" => "Pendente"],
+            ["codigo" => 7, "tipo" => "Consultoria", "status" => "Concluído"],
+            ["codigo" => 4, "tipo" => "Instalação", "status" => "Concluído"]
+        ];  
+
+        $tipoDeServicos = array_column($servicos, "tipo"); 
+        ?>
+
+        <pre><?php var_dump($tipoDeServicos) ?></pre>
+
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
+
 </html>
