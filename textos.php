7ddc23aab179db53f39475b497fdd6f1ea48abe9
Perola-Silva <perolladasillva@gmail.com>
2026-08-19 11:05:45 -0300

    lou

--- textos.php
+++ textos.php
@@ -1,5 +1,7 @@
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis, voluptates dolorem? Rem similique animi, tenetur fuga, voluptatem necessitatibus amet quam iste nostrum quis facilis doloribus odit impedit voluptatibus, esse maiores nisi qui ullam temporibus delectus id dicta ut nihil? Error pariatur aut natus. Error minus inventore sapiente quibusdam quaerat totam?</p>
 
+<?php include "lista.html"; ?>
+
 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores consequatur assumenda, aspernatur, error, temporibus ad nemo aliquid perspiciatis laborum cupiditate velit quis eius ducimus nam atque consequuntur rem placeat! Cupiditate commodi nemo animi quasi sequi in aliquam consequatur! Minima, explicabo!</p>
 
 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum ipsam porro facere expedita quaerat, inventore dolorum voluptate iste quia id.</p>
