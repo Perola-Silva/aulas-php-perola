88ddf2f3f398e5d2f1b61dd28fb64285aa4436b1
Perola-Silva <perolladasillva@gmail.com>
2026-08-20 09:24:20 -0300

    .

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -1,13 +1,28 @@
 <?php
 // Definindo um caminho de base/referencia para os links
 const BASE = "/site/";
+
+// Detectar/guardar o nome da página aberta pelo usuário
+// var_dump($_SERVER['PHP_SELF']);
+// echo "<br>";
+// echo basename($_SERVER['PHP_SELF']);
+
+$pagina = basename($_SERVER['PHP_SELF']);
+
+switch($pagina){
+    case 'index.php': $titulo = "Página Inicial"; break;
+    case 'cursos.php': $titulo = "Cursos"; break;
+    case 'duvidas.php': $titulo = "Duvidas"; break;
+    case 'planos.php': $titulo = "Planos"; break;
+    default: $titulo = "Consultoria"; break;
+}
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site usando PHP</title>
+    <title><?= $titulo ?> - Site usando PHP</title>
     <link rel="stylesheet" href="/CSS/bootstrap.min (1).css">
     <link rel="stylesheet" href="/CSS/styles.css">
 
