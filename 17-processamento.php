546736576d79b271981fc17f5442c8904fe87e58
Perola-Silva <perolladasillva@gmail.com>
2026-08-25 09:08:32 -0300

    modificado tema do bootstrap

--- 17-processamento.php
+++ 17-processamento.php
@@ -4,7 +4,13 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Formulário</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/CSS/bootstrap.min (1).css">
+        <link href="https://fonts.googleapis.com/css2?family=Sour+Gummy:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <style>
+            body{
+                font-family: "Sour Gummy", sans-serif;
+            }
+        </style>
 </head>
 <body>
 <div class="container w-50">
